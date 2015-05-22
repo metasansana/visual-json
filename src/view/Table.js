9aa2f0bb79301b960ae0fea1f7904bedea7bd48b
Lasana Murray <lmurray@quenk.com>
2015-05-22 10:03:44 -0400

    Restore sorting

--- src/view/Table.js
+++ src/view/Table.js
@@ -1,29 +1,16 @@
 import React from 'react';
 import dot from 'dot-component';
 
-/* Utility functions */
-function unshift(val, array) {
-    if (!val) return array;
-    array.unshift(val);
-    return array;
-}
+var compareDate = function(name) {
 
-function push(val, array) {
-    if (!val) return array;
-    return array;
-}
-
-function compareDate(a, b) {
-
-    return function (name) {
-
+    return  function(a, b){
         a = new Date(a[name]).getTime();
         b = new Date(b[name]).getTime();
         return a > b ? -1 : a < b ? 1 : 0;
     }
 }
 
-function compare(name) {
+var compare = function (name) {
 
     return function (a, b) {
 
@@ -36,13 +23,9 @@ function compare(name) {
         if (typeof bval === 'string')
             bval = bval.replace(/\s+/, '').toLowerCase();
 
-
         return (aval > bval) ? -1 : (aval < bval) ? 1 : 0;
     }
 }
-
-
-/* Ende */
 
 /**
  *  Table
@@ -55,149 +38,82 @@ class Table extends React.Component {
 
         this.state = {
             view: '',
-            data: (this.props.data) ? this.props.data.slice() : [],
-            columns: this.props.columns.slice(),
-            shouldReverse: {},
-            sortedOn: ''
+            data: (this.props.data) ? this.props.data : [],
+            columns: (this.props.columns) ? this.props.columns : [],
+            sortedOn: '',
+            lastSorted: '',
+            arrow: ''
         }
 
     }
 
-    headingClicked(name, type) {
+    headingClicked(name, sortAs) {
 
-        var state = {shouldReverse: this.state.shouldReverse, view: 'sorting'};
-        var self = this;
+        var data = this.state.data.slice();
+        var state = {data: data};
 
-        if (type === 'date') {
-            state.data = this.state.data.slice().
-                sort(compareDate(name));
+        //This column was last sorted on this name
+            if (this.state.sortedOn === name) {
+                state.data.reverse();
+                state.lastSortedOn = name;
+                state.arrow = '\u21e7';
 
-        } else {
+            } else if (sortAs === 'date') {
+                state.data = state.data.sort(compareDate(name));
+                state.sortedOn = name;
+                state.arrow = '\u21e9';
+            } else {
+                state.data = state.data.sort(compare(name));
+                state.lastSortedOn = this.state.sortedOn;
+                state.sortedOn = name;
+                state.arrow = '\u21e9';
+            }
 
-            state.data = this.state.data.slice().
-                sort(compare(name));
+            this.setState(state);
 
-        }
-
-        if (!state.shouldReverse.hasOwnProperty(name))
-            state.shouldReverse[name] = false;
-
-        if (state.shouldReverse[name])
-            state.data.reverse();
-
-        state.shouldReverse[name] = !state.shouldReverse[name];
-        state.sortedOn = name;
-
-        self.setState(state, function () {
-            self.setState({view: '', table: self._makeTable()})
-        });
-
-    }
-
-    getArrow(schema) {
-
-        var self = this;
-        var arrow = '';
-
-        if (schema.name === self.state.sortedOn) {
-
-            if (self.state.shouldReverse[schema.name])
-                arrow = '\u21e9';
-
-            if (!self.state.shouldReverse[schema.name])
-                arrow = '\u21e7';
-
-        }
-
-        return arrow;
-
-    }
-
-    _render(tag, props, value) {
-
-        if (!Array.isArray(value))
-            value = [value];
-
-        var args = [];
-
-        args.push(tag);
-        args.push(props);
-        args = args.concat(value);
-        return React.createElement.apply(React, args);
-
-    }
-
-    renderTHEAD() {
-
-        var self = this;
-
-        var headings = self.state.columns.map(function (schema) {
-
-            return self._render('th', {
-                onClick: self.headingClicked.bind(self, schema.name)
-            }, schema.label + '' + self.getArrow(schema));
-
-        });
-
-        if (self.props.appendHeadings)
-            headings.push(_render('th', null, self.props.appendHeadings()));
-
-        return self._render('tr', null, headings);
-
-    }
-
-    renderTBODY() {
-
-        var self = this;
-        var data;
-
-        return self.state.data.map(function (datum, i) {
-
-            var cells = self.props.columns.map(function (column) {
-
-                data = dot.get(datum, column.name);
-
-                if (column.filter)
-                    data = column.filter((column.passAllToFilter)? datum : data);
-
-                return self._render('td', null, data);
-
-            });
-
-            if (self.props.appendCells)
-                cells.push(self._render('td', null, self.props.appendCells(datum)));
-
-            return self._render('tr', null, cells);
-
-
-        });
-    }
-
-    _makeTable() {
-
-        //@todo Optimize, too slow
-        var self = this;
-        var className = 'table ' + ((self.props.className) ? self.props.className : '');
-
-        return self._render('table', {className: className},
-            [self._render('thead', null, self.renderTHEAD()),
-                self._render('tbody', null, self.renderTBODY())]);
-
-    }
-
-    componentDidMount() {
-        this.setState({view: 'show'});
     }
 
     render() {
 
-        if (this.state.view === 'sorting')
-            return React.createElement('b', null, 'Sorting... Please Wait');
+        var self = this;
 
-        if (this.state.table)
-            return this.state.table;
+        var headings = React.createElement('tr', null, self.state.columns.map(function (column, i) {
 
-        return this._makeTable();
+            return React.createElement('th', {
+                onClick: self.headingClicked.bind(self, column.name, column.sortAs),
+                key: i
+            }, column.label, (self.state.sortedOn === column.name)? self.state.arrow: '');
+
+        }));
+
+        var body = self.state.data.map(function (datum, i) {
+
+            return React.createElement('tr', {key: i},
+                self.state.columns.map(function (column, ii) {
+
+                    var data = dot.get(datum, column.name);
+
+                    if (!data)
+                        data = null;
+
+                    if (typeof data === 'object')
+                        data = data.toString();
+
+                    if (column.filter)
+                        data = column.filter(data, datum);
+
+                    return React.createElement('td', {key: ii}, data);
+
+                }));
+
+        });
+
+        return React.createElement('table', {
+                className: 'table ' + ((self.props.className) ? self.props.className : '')
+            },
+            React.createElement('thead', null, headings),
+            React.createElement('tbody', null, body));
+
 
     }
 
