de54dc21a0cb32816a6b589553c0d64f70467094
Lasana Murray <lmurray@quenk.com>
2015-05-25 22:38:21 -0400

    Clean up

--- build/Compiler.js
+++ build/Compiler.js
@@ -207,12 +207,13 @@ var Compiler = (function () {
 
                 this._checkDups(cut(key, '$$filter'), schema);
 
-                schema.filter = (function (filters, ctx) {
+                schema.filter = (function (filters) {
 
                     return function (value, data) {
+                        console.log('filter called with ', value, ' ab data ', data);
                         return self.filter(value, filters, data);
                     };
-                })(schema.$$filter, ctx);
+                })(schema.$$filter);
 
                 delete schema.$$filter;
             }
@@ -249,7 +250,7 @@ var Compiler = (function () {
                 if (!self.filters.hasOwnProperty(nextFilterMethodName)) throw new Error('Unknown filter ' + nextFilterMethodName + '!');
 
                 /* filter(value, args1...argn, context, next)*/
-
+                console.log('push the context ', context);
                 nextFilterArray.unshift(thisValue);
                 nextFilterArray.push(context);
                 nextFilterArray.push(next);
--- src/Compiler.js
+++ src/Compiler.js
@@ -171,12 +171,14 @@ class Compiler {
 
             this._checkDups(cut(key, '$$filter'), schema);
 
-            schema.filter = function (filters, ctx) {
+            schema.filter = function (filters) {
 
                 return function (value, data) {
+                    console.log('filter called with ',value, ' ab data ', data);
                     return self.filter(value, filters, data);
                 }
-            }(schema.$$filter, ctx);
+
+            }(schema.$$filter);
 
             delete schema.$$filter;
 
@@ -213,7 +215,7 @@ class Compiler {
                 throw new Error('Unknown filter ' + nextFilterMethodName + '!');
 
             /* filter(value, args1...argn, context, next)*/
-
+console.log('push the context ', context);
             nextFilterArray.unshift(thisValue);
             nextFilterArray.push(context);
             nextFilterArray.push(next);
