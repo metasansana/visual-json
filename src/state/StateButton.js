2838972f86bc0f9da8d7e3f9dd72c4f63726fae3
Lasana Murray <lmurray@quenk.com>
2015-06-11 12:01:37 -0400

    Add some docs on props

--- src/state/StateButton.js
+++ src/state/StateButton.js
@@ -5,24 +5,33 @@ import Button from '../form/Button';
  * StateButton is used to manipulate state of a context object via the click of a button.'
  *
  * Whatever you supply as the `resultKey` prop, will be set to the value of the buttons name.
- * This is typically usefull when used in combination with a Switch component.
+ * This is typically useful when used in combination with a Switch component.
+ *
  */
 class StateButton extends Button {
 
-  buttonClicked(e) {
-    var state = {};
-    state[this.props.stateKey] = this.props.name;
-    this.props.target.setState(state);
-  }
+    buttonClicked(e) {
+        var state = {};
+        state[this.props.stateKey] = this.props.name;
+        this.props.target.setState(state);
+    }
 }
 
+/**
+ * name {String} The name of this button.
+ * target {Object} The object setState() will be called on.
+ * stateKey {String} The key name for the setState() call.
+ * textLabel {String} The text label if the button
+ * icon {String} If set, it will generate a font awesome icon in the button with this value as the class.
+ * className {String} The class name of the button.
+ */
 StateButton.propTypes = {
-  name: React.PropTypes.string.isRequired,
-  target: React.PropTypes.object.isRequired,
-  stateKey: React.PropTypes.string.isRequired,
-  textLabel: React.PropTypes.string,
-  icon: React.PropTypes.string,
-  className:React.PropTypes.string
+    name: React.PropTypes.string.isRequired,
+    target: React.PropTypes.object.isRequired,
+    stateKey: React.PropTypes.string.isRequired,
+    textLabel: React.PropTypes.string,
+    icon: React.PropTypes.string,
+    className: React.PropTypes.string
 
 };
 
