84f4dc666029f814676601d3661266f9a493cb51
Lasana Murray <lmurray@quenk.com>
2015-07-03 21:55:15 -0400

    ResourceTraversal

---
+++ src/core/ResourceTraversal.js
@@ -0,0 +1,60 @@
+import Utils from './Utils';
+
+/**
+ * ResourceTraversal traverses a `$resource` directive in the schema.
+ *
+ * A request for the resource is executed at $parse time and the resulting response is put
+ * into scope.
+ *
+ *  @example <caption>Example schema</caption>:
+ *   "$resource": {
+ *      "person": {
+ *        "url": "/api/persons",
+ *        "method": "GET"
+ *       }
+ *   }
+ * The $resource schema must have only one property, this property is used as
+ * the name of the resource when adding to scope.
+ *
+ * @todo Provide way to read links and make requests available from them.
+ * @param {ResourceDirective} request An instance of RequestDirective used to make the request.
+ * @param {ParseScope} scope The to add the resource to.
+ */
+class ResourceTraversal {
+
+    constructor(request, scope) {
+        this.request = request;
+        this.scope = scope;
+    }
+
+    _makeLinkFunction(link) {
+        return function () {
+            return this.request.send(link);
+        }
+    }
+
+    traverse(tree, done) {
+
+        this.request.send(tree.request).
+            then(function (res) {
+
+                var data = res.data || res.body;
+                var links = Utils.createSafeMap();
+
+                if (tree.links)
+                    if (Array.isArray(data.links))
+                        for (var key in tree.links)
+                            data.links.forEach(function (link) {
+                                if (link.rel === key)
+                                    links[key] = this._makeLinkFunction(Utils.merge(link, tree.links[key]));
+                            }.bind(this));
+
+                this.scope.addToResource(tree.name, {data: data, links: links});
+                done();
+
+            }.bind(this));
+    }
+
+}
+
+export default ResourceTraversal
